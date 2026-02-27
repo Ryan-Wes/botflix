94e4e6105b6892520ba3830df4c4ebb9c31229b4
WRyan <wesley.ryan03@gmail.com>
2026-02-27 20:11:13 -0300

    add fetch webhook to n8n

--- src/js/index.js
+++ src/js/index.js
@@ -6,13 +6,36 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function setupEventListeners() {
+
+    moodTextArea.addEventListener('keypress', (event) => {
+        if (event.key === 'Enter' && !event.shiftKey) {
+            event.preventDefault();
+            handleSearch();
+        }
+    });
+
     // Adiciona um ouvinte de evento para o clique no botão de busca
     searchButton.addEventListener('click', handleSearch);
 }
 
-function handleSearch() {
+async function handleSearch() {
     const mood = moodTextArea.value.trim();
     if (!mood) {
         alert('Por favor, descreva seu humor para encontrar o filme perfeito!');
         return;
-    }   
+    }
+
+    const response = await fetch('https://wryan.app.n8n.cloud/webhook-test/botflix', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ userPrompt: mood })
+    });
+
+    
+};
+
+
+
+
